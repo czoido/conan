32d0a772bd655be8dc6cc0fe81d822be666940cb
czoido <mrgalleta@gmail.com>
2020-07-06 11:08:27 +0200

    add test

--- conans/test/functional/build_helpers/cmake_configs_test.py
+++ conans/test/functional/build_helpers/cmake_configs_test.py
@@ -1,5 +1,6 @@
 import os
 import unittest
+from textwrap import dedent
 
 from nose.plugins.attrib import attr
 
@@ -19,6 +20,59 @@ class CMakeConfigsTest(unittest.TestCase):
         client.run("create . user/testing")
         self.assertIn("Hello Release Hello0", client.out)
         self.assertIn("Hello Debug Hello0", client.out)
+
+    def test_package_configs_del_compiler(self):
+        client = TestClient()
+
+        hello_cpp = dedent("""
+            #include <iostream>
+    
+            void main(){
+                #ifndef _DEBUG
+                std::cout << "Hello World Release!" <<std::endl;
+                #else
+                std::cout << "Hello World Debug!" <<std::endl;
+                #endif
+            }
+            """)
+
+        conanfile = dedent("""
+            from conans import ConanFile, CMake
+            import os  
+        
+            class Pkg(ConanFile):
+                exports = '*'
+                generators = "cmake"
+                settings = "os", "compiler", "build_type", "arch"
+                
+                def configure(self):
+                    del self.settings.compiler
+    
+                def build(self):
+                    cmake = CMake(self)
+                    cmake.configure()
+                    cmake.build()
+                    os.chdir("bin")
+                    self.run(".%shello" % os.sep)
+            """)
+
+        cmakelists_txt = dedent("""
+            cmake_minimum_required(VERSION 2.8)
+            project(myhello CXX)
+            
+            include(${CMAKE_BINARY_DIR}/conanbuildinfo.cmake)
+            conan_basic_setup()
+            
+            add_executable(hello hello.cpp)        
+        """)
+
+        client.save({"conanfile.py": conanfile,
+                     "hello.cpp": hello_cpp,
+                     "CMakeLists.txt": cmakelists_txt})
+
+        client.run("create . hello/1.0@ --settings build_type=Release")
+        self.assertNotIn("Hello World Debug!", client.out)
+        self.assertIn("Hello World Release!", client.out)
 
     def cmake_multi_test(self):
         client = TestClient()
