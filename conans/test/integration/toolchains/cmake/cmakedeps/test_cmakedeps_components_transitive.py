32ca920f3cdacb952c454024163138532c62bdff
czoido <mrgalleta@gmail.com>
2022-07-07 14:03:56 +0200

    wip

--- conans/test/integration/toolchains/cmake/cmakedeps/test_cmakedeps_components_transitive.py
+++ conans/test/integration/toolchains/cmake/cmakedeps/test_cmakedeps_components_transitive.py
@@ -1,3 +1,4 @@
+import os
 import platform
 import textwrap
 
@@ -6,25 +7,32 @@ from conans.test.utils.tools import TestClient
 
 
 def test_cmakedeps_propagate_components():
+    """
+    lib_a: has two components cmp1, cmp2, cmp3
+    lib_b --> libA cmp1
+    lib_c --> libA cmp2
+    consumer --> libB, libC
+    """
     client = TestClient()
-    top = textwrap.dedent("""
+    lib_a = textwrap.dedent("""
         import os
         from conan import ConanFile
         from conan.tools.files import copy
 
-        class TopConan(ConanFile):
-            name = "top"
+        class lib_aConan(ConanFile):
+            name = "lib_a"
             version = "1.0"
             settings = "os", "compiler", "build_type", "arch"
             exports_sources = "include/*"
 
             def package(self):
                 copy(self, "*.h", os.path.join(self.source_folder, "include"),
-                                   os.path.join(self.package_folder, "include"))
+                                  os.path.join(self.package_folder, "include"))
 
             def package_info(self):
                 self.cpp_info.components["cmp1"].includedirs = ["include"]
                 self.cpp_info.components["cmp2"].includedirs = ["include"]
+                self.cpp_info.components["cmp3"].includedirs = ["include"]
         """)
 
     cmp_include = textwrap.dedent("""
@@ -34,50 +42,80 @@ def test_cmakedeps_propagate_components():
         """)
 
     client.save({
-        'top/conanfile.py': top,
-        'top/include/cmp1.h': cmp_include.format(cmpname="cmp1"),
-        'top/include/cmp2.h': cmp_include.format(cmpname="cmp2"),
+        'lib_a/conanfile.py': lib_a,
+        'lib_a/include/cmp1.h': cmp_include.format(cmpname="cmp1"),
+        'lib_a/include/cmp2.h': cmp_include.format(cmpname="cmp2"),
+        'lib_a/include/cmp3.h': cmp_include.format(cmpname="cmp3"),
     })
 
-    client.run("create top")
+    client.run("create lib_a")
 
-    middle = textwrap.dedent("""
+    lib = textwrap.dedent("""
         import os
         from conan import ConanFile
         from conan.tools.files import copy
 
 
-        class MiddleConan(ConanFile):
-            name = "middle"
+        class lib_{name}Conan(ConanFile):
+            name = "lib_{name}"
             version = "1.0"
-            requires = "top/1.0"
             settings = "os", "compiler", "build_type", "arch"
             exports_sources = "include/*"
 
+            def requirements(self):
+                self.requires("lib_a/1.0", components={components})
+
             def package(self):
                 copy(self, "*.h", os.path.join(self.source_folder, "include"),
-                                   os.path.join(self.package_folder, "include"))
+                                  os.path.join(self.package_folder, "include"))
 
-            def package_info(self):
-                self.cpp_info.requires = ["top::cmp1"]
         """)
 
-    middle_include = textwrap.dedent("""
+    lib_include = textwrap.dedent("""
         #pragma once
         #include <iostream>
-        #include "cmp1.h"
-        void middle(){ cmp1(); };
+        #include {include}
+        void lib_{name}(){{ {components} }};
         """)
 
 
     client.save({
-        'middle/conanfile.py': middle,
-        'middle/include/middle.h': middle_include,
+        'lib_b/conanfile.py': lib.format(name="b", components='["cmp1"]'),
+        'lib_b/include/lib_b.h': lib_include.format(name="b", include='"cmp1.h"', components='cmp1();'),
     })
 
-    client.run("create middle")
+    client.save({
+        'lib_c/conanfile.py': lib.format(name="c", components='["cmp2"]'),
+        'lib_c/include/lib_c.h': lib_include.format(name="c", include='"cmp2.h"', components='cmp2();'),
+    })
 
-    client.run("install middle/1.0@ -g CMakeDeps")
+    client.run("create lib_b")
 
-    assert "top::cmp2" not in client.load("top-release-x86_64-data.cmake")
-    assert "top::cmp2" not in client.load("top-Target-release.cmake")
+    client.run("create lib_c")
+
+    consumer = textwrap.dedent("""
+    from conan import ConanFile
+
+    class ConsumerConan(ConanFile):
+        name = "consumer"
+        version = "1.0"
+        settings = "os", "compiler", "build_type", "arch"
+        generators = "CMakeDeps"
+        def requirements(self):
+            self.requires("lib_b/1.0")
+            self.requires("lib_c/1.0")
+    """)
+
+    client.save({'consumer/conanfile.py': consumer})
+
+    client.run("install consumer")
+
+    """
+    By default if you specify self.requires("lib_a/1.0", components=["cmp1"])
+    That would go as well to the self.cpp_info.requires and mean: self.cpp_info.requires = ["top::cmp1"]
+    But there are cases that you may require several components but decide to only make available for
+    consumers with the cpp_info some of them
+    """
+
+    assert "lib_a::cmp2" not in client.load(os.path.join("consumer", "lib_a-release-x86_64-data.cmake"))
+    assert "lib_a::cmp2" not in client.load(os.path.join("consumer", "lib_a-Target-release.cmake"))
