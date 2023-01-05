8814d0b36386fe28862fc91608b5a20b7a12ef2c
czoido <mrgalleta@gmail.com>
2023-01-05 14:26:17 +0100

    wip

--- conans/test/integration/ui/test_progress_bars.py
+++ conans/test/integration/ui/test_progress_bars.py
@@ -1,10 +1,24 @@
+import textwrap
+
 from conans.test.assets.genconanfile import GenConanfile
 from conans.test.utils.tools import TestClient
 
+conanfile = textwrap.dedent("""
+        from conan import ConanFile
+        from conan.tools.files import copy, get
+
+        class PkgConan(ConanFile):
+            name = "mylib"
+            version = "1.0"
+
+            def source(self):
+                get(self, "https://github.com/adamstark/AudioFile/archive/1.1.0.tar.gz",
+                    destination=self.source_folder, strip_root=True)
+    """)
 
 def test_basic_parallel_download():
     client = TestClient(default_server_user=True)
-    client.save({"conanfile.py": GenConanfile().with_exports_sources("*")})
+    client.save({"conanfile.py": conanfile})
     client.run("create . --name=mylib --version=1.0")
     client.run("upload * --confirm -r default")
     client.run("remove * -c")
