b3e3ac0971c38c1d7015147ccbc3712e02a8a4dc
Carlos Zoido <mrgalleta@gmail.com>
2026-04-27 12:30:54 +0200

    wip

--- conan/internal/model/conf.py
+++ conan/internal/model/conf.py
@@ -144,6 +144,7 @@ BUILT_IN_CONFS = {
     "tools.system.package_manager:sudo_askpass": "Use the '-A' argument if using sudo in Linux to invoke the system package manager (False by default)",
     "tools.system.pyenv:python_interpreter": "(Experimental) Path to the Python interpreter to be used to create the virtualenv",
     "tools.system.pipenv:python_interpreter": "(Deprecated) Use 'tools.system.pyenv:python_interpreter' instead. Path to the Python interpreter to be used to create the virtualenv",
+    "tools.system.condaenv:micromamba_path": "(Experimental) Path to a micromamba executable to be used by CondaEnv. If unset, CondaEnv looks for 'micromamba' on PATH",
     "tools.system.condaenv:root_prefix": "(Experimental) Root directory for the conda package cache shared across recipes",
     "tools.apple:sdk_path": "Path to the SDK to be used",
     "tools.apple:enable_bitcode": "(boolean) Enable/Disable Bitcode Apple Clang flags",
